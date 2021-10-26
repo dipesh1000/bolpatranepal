c09c809309a1c17d169155d7d3692e4b70aee0a0
Rozin200 <39566227+rozin200@users.noreply.github.com>
2021-10-26 18:09:00 +0545

    features

--- src/components/AboutUsPagesComponents/Features.js
+++ src/components/AboutUsPagesComponents/Features.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+import FeatureContent from './FeatureContent'
 
 const Features = () => {
     return (
-        <div className="features">
-            
-        </div>
+        <Container className="holder">
+            <FeatureContent logo={<i class="fa fa-newspaper-o fa-2x" aria-hidden="true"></i>} title="Latest Procurement News"
+                content="A platform for all procurement activities of government of Nepal. Lorem ipsum dolor sit amet a platform for all procurement activities of Government of Nepal." />
+            <FeatureContent logo={<i class="fa fa-envelope-o fa-2x" aria-hidden="true"></i>} title="Latest Procurement News"
+                content="A platform for all procurement activities of government of Nepal. Lorem ipsum dolor sit amet a platform for all procurement activities of Government of Nepal." />
+            <FeatureContent logo={<i class="fa fa-file-video-o fa-2x" aria-hidden="true"></i>} title="Latest Procurement News"
+                content="A platform for all procurement activities of government of Nepal. Lorem ipsum dolor sit amet a platform for all procurement activities of Government of Nepal." />
+        </Container>
     )
 }
 
