fe757e5111dd9dc0e4e2b85b306e25bccb836791
Rozin200 <39566227+rozin200@users.noreply.github.com>
2021-10-27 13:03:51 +0545

    features flex-box

--- src/components/AboutUsPagesComponents/FeatureContent.js
+++ src/components/AboutUsPagesComponents/FeatureContent.js
@@ -3,10 +3,12 @@ import { Container } from 'react-bootstrap'
 
 const FeatureContent = ({ logo, title, content }) => {
     return (
-        <div className="featureContent_container">
-            <div className="icon">{logo}</div>
-            <h4 className="title">{title}</h4>
-            <div className="content">{content}</div>
+        <div className="feature_content">
+            <div className="featureContent_container">
+                <div className="icon">{logo}</div>
+                <h4 className="title">{title}</h4>
+                <div className="content">{content}</div>
+            </div>
         </div>
     )
 }
